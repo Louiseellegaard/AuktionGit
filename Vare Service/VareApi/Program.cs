8ee71f9be05aaf088b7c83a1021101db8342fc20
Mikkel <eaamiby@students.eaaa.dk>
2022-10-31 16:15:17 +0100

    Added CORS

--- Vare Service/VareApi/Program.cs
+++ Vare Service/VareApi/Program.cs
@@ -4,6 +4,16 @@ using VareApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var AllowSomeStuff = "_AllowSomeStuff";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowSomeStuff, builder => {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
