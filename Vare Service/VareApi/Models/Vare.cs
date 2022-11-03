feab4c1a1b476921e659568b42a03c8d8f41a264
Mikkel <eaamiby@students.eaaa.dk>
2022-11-03 13:25:18 +0100

    Rettet navn

--- Vare Service/VareApi/Models/Vare.cs
+++ Vare Service/VareApi/Models/Vare.cs
@@ -9,7 +9,7 @@ public class Vare
     public string ProductId { get; set; }   
     public string? Title { get; set; }
     public string? Description { get; set; }
-    public int ShowRoomid { get; set; }
+    public int ShowRoomId { get; set; }
     public double Valuation { get; set; }
     public string AuctionStart { get; set; }
     public string[]? Images { get; set; }
