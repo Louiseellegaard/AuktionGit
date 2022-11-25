ac76ec382fe0c3800c3743c3335aaee03bb75227
Mikkel <eaamiby@students.eaaa.dk>
2022-11-25 10:58:09 +0100

    Tilføjet caching til Get

--- Kunde Service/KundeApi/Controllers/KundeController.cs
+++ Kunde Service/KundeApi/Controllers/KundeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
 using KundeApi.Models;
 using KundeApi.Services;
 
@@ -10,11 +12,13 @@ public class KundeController : ControllerBase
 {
     private readonly ILogger<KundeController> _logger;
     private readonly IDataService _dataService;
+    private readonly IMemoryCache _memoryCache;
 
-    public KundeController(ILogger<KundeController> logger, IDataService dataService)
+    public KundeController(ILogger<KundeController> logger, IDataService dataService, IMemoryCache memoryCache)
     {
         _logger = logger;
         _dataService = dataService;
+        _memoryCache = memoryCache;
     }
 
     [HttpGet("version")]
@@ -45,16 +49,22 @@ public class KundeController : ControllerBase
     [HttpGet("{id}", Name = "Get")]
     public async Task<ActionResult<Kunde>> Get(string id)
     {
-        var kunde = await _dataService
-            .Get(id);
+		var kunde = GetCustomerFromCache(id);
 
-        if(kunde is null)
+        if (kunde is null)
         {
-            return NotFound();
-        }
+			kunde = await _dataService
+				.Get(id);
 
-        return kunde;
-    }
+			if (kunde is null)
+			{
+				return NotFound();
+			}
+
+			SetCustomerInCache(kunde);
+		}
+		return Ok(kunde);
+	}
 
 	// POST: api/Kunde
 	[HttpPost]
@@ -75,6 +85,29 @@ public class KundeController : ControllerBase
 			.Create(kunde);
 
 		return CreatedAtRoute("Get", new { id = kunde.KundeId }, kunde);
+	}
+
+	private void SetCustomerInCache(Kunde kunde)
+	{
+		var cacheExpiryOptions = new MemoryCacheEntryOptions
+		{
+			AbsoluteExpiration = DateTime.Now.AddHours(1),
+			SlidingExpiration = TimeSpan.FromMinutes(10),
+			Priority = CacheItemPriority.High
+		};
+		_memoryCache.Set(kunde.KundeId, kunde, cacheExpiryOptions);
+	}
+
+	private Kunde GetCustomerFromCache(string id)
+	{
+		Kunde kunde = null;
+		_memoryCache.TryGetValue(id, out kunde);
+		return kunde;
+	}
+
+	private void RemoveFromCache(string id)
+	{
+		_memoryCache.Remove(id);
 	}
 
 	public record KundeDTO(string Name, string PhoneNumber, string Email, string City, int ZipCode, string Country, string Address);
