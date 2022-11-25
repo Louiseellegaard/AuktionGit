b0b481d46e7e782a5c2f0cf134094dbc6ed7591a
Mikkel <eaamiby@students.eaaa.dk>
2022-11-25 21:06:43 +0100

    Tilføjet VareListe-page

---
+++ Vare Service/VareApi/Pages/VareListe.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VareApi.Models;
+
+namespace MyApp.Namespace
+{
+	public class VareListeModel : PageModel
+	{
+		public List<Vare>? VareListe { get; set; }
+
+		public void OnGet()
+		{
+			using HttpClient client = new()
+			{
+				BaseAddress = new Uri("http://localhost:80/")
+			};
+
+			// Henter vareliste. 
+			VareListe = client.GetFromJsonAsync<List<Vare>>("api/vare").Result;
+		}
+	}
+
+}
