bdd696782eb3b9d5da5dc9ac04e0f02e1d987337
Mikkel <eaamiby@students.eaaa.dk>
2022-12-05 15:38:22 +0100

    Tilføjet logging

--- Vare Service/VareService.API/Services/DbContext.cs
+++ Vare Service/VareService.API/Services/DbContext.cs
@@ -1,6 +1,5 @@
-﻿using MongoDB;
-using MongoDB.Driver;
-using System;
+﻿using MongoDB.Driver;
+
 using VareService.Models;
 
 namespace VareService.Services
@@ -12,21 +11,26 @@ namespace VareService.Services
 
     public class DbContext : IDbContext
     {
-        public DbContext()
+		private ILogger<DbContext> _logger;
+		public IMongoCollection<Vare> VareCollection { get; }
+
+		public DbContext(ILogger<DbContext> logger)
         {
-            var connectionString = "mongodb+srv://louisedb:louisedb123@auktionshusdb.upg5v0d.mongodb.net/?retryWrites=true&w=majority";
+			_logger = logger;
 
-            // Opretter en MongoDB-client med forbindelse til MongoDB Atlas
-            var client = new MongoClient(connectionString);
+			var _connectionString = "mongodb+srv://louisedb:louisedb123@auktionshusdb.upg5v0d.mongodb.net/?retryWrites=true&w=majority";
 
-            // Henter auktions-databasen fra client
-            var _mongoDatabase = client.GetDatabase("Auktiondb");
+			// Opretter en 'MongoClient' med forbindelse til MongoDB Atlas
+			var _client = new MongoClient(_connectionString);
 
-            VareCollection = _mongoDatabase.GetCollection<Vare>("Vare");
-        }
+			// Henter auktions-databasen fra '_client'
+			var _mongoDatabase = _client.GetDatabase("Auktiondb");
 
-        // Henter varer fra _mongoDatabase ("Vare")
-        public IMongoCollection<Vare> VareCollection { get; }
+			// Henter vare-collection fra '_mongoDatabase'
+			VareCollection = _mongoDatabase.GetCollection<Vare>("Vare");
 
+			_logger.LogInformation("Forbundet til database {database}", _mongoDatabase);
+			_logger.LogInformation("Benytter collection {collection}", VareCollection);
+		}
     }
 }
