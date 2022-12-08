85839dfcaba1bafd3f699e3a7d904ba3a8a10df3
Alexander Jensen <alexanderjensen@MacBook-Air-tilhrende-Alexander.local>
2022-12-08 14:54:15 +0100

    h

--- AuktionService/AuktionService.API/Models/Auktion.cs
+++ AuktionService/AuktionService.API/Models/Auktion.cs
@@ -10,7 +10,6 @@ public class Auktion
 	public string? AuctionId { get; set; }
     public string? ProductId { get; set; }
     public string? BuyerId { get; set; }
-    public string? Catagory { get; set; }
     public string? Description { get; set; }
     public DateTime EndTime { get; set; }
     public double MinimumPrice { get; set; }
